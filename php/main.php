bfad99f83a959802eda3885282696ed1fc900a4b
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-07-04 13:16:19 -0600

    Normalizacion de archivo

---
+++ php/main.php
@@ -0,0 +1,29 @@
+<?php
+//          ---+++++     consumo de API "Frase del motivadora"     +++++---
+
+// Usando cURL
+$ch = curl_init();      // iniciamos el cURL
+
+// Configuramos la URL
+curl_setopt($ch, CURLOPT_URL, "https://frasedeldia.azurewebsites.net/api/phrase");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Ejecutamos la solicitud y Almacenamos la respuesta
+$response =  curl_exec($ch);
+
+// Cerramos cURL
+curl_close($ch);
+
+// Decodificamos la respuesta JSON
+$data = json_decode($response, true);
+
+// Verificamos si la decodificacion tuvo exito
+if(json_last_error() === JSON_ERROR_NONE){
+    $phrase = $data ['phrase'] ?? 'NO phrase funnd';
+    $author = $data['author'] ?? 'Unknown author funnd';
+    echo '<p class = "caja" id = "frase" >' . "Phrase: $phrase " . "<br>" . " Author: $author" . "</p>";
+    echo '<br>' . '<br>';
+}else{
+    echo "Failed to decode JSON response";
+}
+?>
