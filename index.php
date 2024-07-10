74be773d6f086bc220c79d5d1c9170fd6e032eb4
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-07-09 18:10:33 -0600

    Modificaciones paraa mejorar  el footer

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style/style.css">
+    <script src = "js/actualizacion.js"></script>
     <title>Frase del dia</title>
 </head>
 <body>
+    <header>
+        <h1>Encabezado del sitio</h1>
+    </header>
 
     <div class = "seccion-principal">
         <h1>La frase del dia es</h1>
@@ -24,9 +28,13 @@
         <button class = "boton enviar" id = "enviar">Enviar</button>
     </div>
 
-    <footer class = "fooster">
-        <!-- <p class = "text-center">Consumo de la API "frase del dia"</p> -->
-        Consumo de la API "frase del dia"  © 2024
+    <footer class = "footer">
+        Consumo de la API "frase del dia"  © 2024 <p id = "actualizacion-fecha"></p>
+        <br>
+        <p class = "fecha-hora" id = "actualizacion-fecha"></p>
+        <p class = "fecha-hora" id = "dame-hora"></p>
     </footer>
+
+    
 </body>
 </html>
