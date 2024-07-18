49fa6a2555039216e537e42fe82e6f4724a8809b
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-07-18 11:13:57 -0600

    Se elimina el codigo JS y se hace refencia a donde se encuntra

--- index.php
+++ index.php
@@ -31,23 +31,7 @@
         </div>
     </main>
 
-    <script>
-        document.getElementById('buscar').addEventListener('click', function(){
-            var xhr = new XMLHttpRequest();
-            // cremos la solicitud
-            xhr.open('GET', 'php/main.php', true);
-            // manejamos la solicitud
-            xhr.onload = function(){
-                if(xhr.status === 200){
-                    console.log('PHP ejecutado correctamente' , xhr.responseText);
-                    document.getElementById('respuesta').innerHTML = xhr.responseText;
-                }else{
-                    console.error('Error al ejecutar PHP', xhr.status, xhr.statusText);
-                }
-            };
-            xhr.send();
-        });
-    </script>
+    <script src = "js/resultados.js"></script>
 
     <footer class = "footer">
         Consumo de la API "frase del dia" ©
